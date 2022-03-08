5e593583690ad1615d11e6456eb7b905f942ded4
rolando22 <rolando2207@gmail.com>
2022-03-08 16:25:41 -0300

    Encapsulado las fórmulas de las funciones

--- figuras.js
+++ figuras.js
@@ -1,32 +1,36 @@
 // Código del cuadrado
 
 console.group("Cuadrados");
-const ladoCuadrado = 5;
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`);
-console.log(`El perímetro del cuadrado es: ${ladoCuadrado*4}cm`);
-console.log(`El área del cuadrado es: ${ladoCuadrado*ladoCuadrado}cm^2`);
+function perimetroCuadrado (lado) {
+    return lado * 4;
+}
+function areaCuadrado (lado) {
+    return lado * lado;
+}
 console.groupEnd();
 
 // Código del triángulo
 
 console.group("Triángulos");
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = Math.sqrt((baseTriangulo/2)^2+ladoTriangulo1^2);
-console.log(`Los lados del triángulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm, ${baseTriangulo}cm`);
-console.log(`La altura del triángulo es: ${alturaTriangulo}cm`);
-console.log(`El perímetro del triángulo es: ${ladoTriangulo1+ladoTriangulo2+baseTriangulo}cm`);
-console.log(`El área del triángulo es: ${baseTriangulo*alturaTriangulo/2}cm^2`)
+function perimetroTriangulo (lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+function areaTriangulo (lado1, base) {
+    var altura = Math.sqrt((base / 2) ^ 2 + lado1 ^ 2);
+    return base * altura / 2; 
+}
 console.groupEnd();
 
 // Código del círculo
 
 console.group("Círculos");
-const radioCirculo = 4;
-const diametroCirulo = radioCirculo*2;
-console.log(`El radio del círculo es: ${radioCirculo}cm`);
-console.log(`El diámetro del círculo es: ${diametroCirulo}cm`);
-console.log(`El perímetro del círculo es: ${diametroCirulo*Math.PI}cm`);
-console.log(`El área del círculo es: ${radioCirculo*radioCirculo*Math.PI}cm^2`);
+function diametroCirculo (radio) {
+    return radio * 2;
+}
+function perimetroCirculo (radio) {
+    return diametroCirculo(radio) * Math.PI;
+}
+function areaCirculo (radio) {
+    return radio * radio * Math.PI;
+}
 console.groupEnd();
