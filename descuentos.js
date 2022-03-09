2d1a453a7ff6458499c16ba0b4c462efd80682cf
rolando22 <rolando2207@gmail.com>
2022-03-09 13:55:52 -0300

    Agrego validación de cupos

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,5 @@
-const coupons = [10, 25, 50];
-function calcularPrecioConDescuento (precio, descuento) {
+const coupons = [{name: "Magic", discount: 10}, {name: "Arena", discount: 25}, {name: "Amazon", discount: 50}];
+function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = precio * porcentajePrecioConDescuento / 100;
     return precioConDescuento;
@@ -7,9 +7,17 @@ function calcularPrecioConDescuento (precio, descuento) {
 function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("input-price");
     const priceValue = inputPrice.value;
-    const coupon = document.getElementById("coupons");
-    const couponValue = parseInt(coupon.value);
-    priceDiscount = calcularPrecioConDescuento(priceValue, coupons[couponValue]);
-    const resultPrice = document.getElementById("result-price");
-    resultPrice.innerText = `El precio con descuento son $${priceDiscount}`;
+    const coupon = document.getElementById("input-coupon");
+    const couponValue = coupon.value;
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    }
+    const couponUser = coupons.find(isCouponValueValid);
+    if (!couponUser) {
+        alert(`El cupon ${couponValue} no es valido`);
+    } else {
+        priceDiscount = calcularPrecioConDescuento(priceValue, couponUser.discount);
+        const resultPrice = document.getElementById("result-price");
+        resultPrice.innerText = `El precio con descuento son $${priceDiscount}`;
+    }
 }
